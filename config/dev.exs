089eb2f33b753877fc82a9085ee98da3e94f25ba
bo0tzz <btdewilligen@gmail.com>
2022-03-26 14:07:43 +0100

    Set up CSS

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,8 @@ config :spoor_kapot, SpoorKapotWeb.Endpoint,
   secret_key_base: "8VfLGoCt+zVwEgwzY4QKIiQB8Z4f1UPuYex/Cu+zjzvzrsUMej53NyKmwzjyicPQ",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule SpoorKapot.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      {:tailwind, "~> 0.1", only: :dev}
     ]
   end
 
@@ -56,7 +58,7 @@ defmodule SpoorKapot.MixProject do
   defp aliases do
     [
       setup: ["deps.get"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
