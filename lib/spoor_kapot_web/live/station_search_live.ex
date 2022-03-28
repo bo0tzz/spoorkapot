263b9a8545763b3b3595ba2b3f4752dcbd1a5a71
bo0tzz <btdewilligen@gmail.com>
2022-03-28 02:00:48 +0200

    Liveview component with autocomplete station search

---
+++ lib/spoor_kapot_web/live/station_search_live.ex
@@ -0,0 +1,41 @@
+defmodule SpoorKapotWeb.StationSearchLive do
+  use SpoorKapotWeb, :live_view
+  require Logger
+
+  def mount(_params, _session, socket) do
+    assigns = [
+      conn: socket,
+      search_results: search("amst"),
+      search_phrase: "amst",
+      selected_stations: []
+    ]
+
+    {:ok, assign(socket, assigns)}
+  end
+
+  def handle_event("search", %{"search_phrase" => search_phrase}, socket) do
+    assigns = [
+      search_results: search(search_phrase),
+      search_phrase: search_phrase
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event(
+        "pick",
+        %{"name" => name, "code" => code},
+        %{assigns: %{selected_stations: selected}} = socket
+      ) do
+    assigns = [
+      search_results: [],
+      search_phrase: "",
+      selected_stations: [{name, code} | selected]
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def search(""), do: []
+  def search(q), do: SpoorKapot.StationSearch.find_stations(q)
+end
