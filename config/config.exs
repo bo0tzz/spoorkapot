0e460be7fa5617ce8535bbc1600ddd4f6b355165
bo0tzz <btdewilligen@gmail.com>
2022-03-29 18:26:14 +0200

    Set hackney as tesla adapter

--- config/config.exs
+++ config/config.exs
@@ -48,6 +48,8 @@ config :tailwind,
     cd: Path.expand("../assets", __DIR__)
   ]
 
+config :tesla, adapter: Tesla.Adapter.Hackney
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
